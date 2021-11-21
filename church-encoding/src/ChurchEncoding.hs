345f159bf2d8e129b794c2b47fd1858cf7ef20bf
Maria Alejandra Vélez Clavijo <68791969+malejav02@users.noreply.github.com>
2021-11-21 10:43:01 -0500

    Add files via upload
    Changes in the implementation of arithmetic operations

--- church-encoding/src/ChurchEncoding.hs
+++ church-encoding/src/ChurchEncoding.hs
@@ -15,42 +15,28 @@ internalchurch n =
 church :: Natural->Expr 
 church n = Lam "f" (Lam "x" (internalchurch n))
 
-successor:: Natural -> Expr 
-successor n = betared (App  (Lam "n"(App (Var "f")(Var "x"))) 
-     (App (Var"f")(App (Var"n")(App (Var"f")(Var "x")))))
+successor:: Expr -> Expr 
+successor n =Lam "n" (Lam "f" (Lam "x"(App(Var "f")(
+    App(App(Var "n")(Var "f"))(Var "x")))))
 
-add:: Natural -> Natural -> Expr 
-add m n = betared 
-    (App
-    (Lam "m" (App (Var "n") (App (Var "f")(Var "x")) ))
-    (App (Var "m")(App (Var "f")(App (Var "n")(App(Var "f")(Var "x")))))
-    )
+add:: Expr -> Expr -> Expr 
+add m n =Lam "m" (Lam "n" (Lam "f" (Lam "x" (
+    App (App (Var "m")(Var "f"))
+    (App (App (Var "n")(Var "f"))(Var "x"))))))
 
-mult:: Natural->Natural->Expr 
-mult m n= betared(
-    App 
-     (Lam "m" (App (Var "n") (App (Var "f")(Var "x")) ))
-    (App(App(Var "m")(App (Var "n")(Var "f")))(Var "x")))
+mult:: Expr->Expr->Expr 
+mult m n=
+    Lam "m" (Lam "n" (Lam "f" (Lam "x" 
+    (App (App (Var "m")(App (Var "n")(Var "f"))) (Var "x")))))
+exp:: Expr ->Expr -> Expr 
+exp m n= 
+  Lam "m" (Lam "n" (Lam "f" (Lam "x" 
+  (App (App (App (Var "n")(Var "m"))(Var "f"))(Var "x")))))
 
-
-exp:: Natural -> Natural -> Expr 
-exp m n= betared(
-    App
-    (Lam "m" (App (Var "n") (App (Var "f")(Var "x")) ))
-    (App (App (Var "n")(Var "m"))(App (Var "f")(Var "x"))))
-
-
-pred:: Natural->Expr 
-pred n = betared(
-    App
-    (Lam "n"(App (Var "f")(Var "x")))
-   (App 
-   
-    (App
-    (
-        App (Var "g")(Lam "g" 
-        (Lam "h" (App(Var "h")
-        (App(Var "g")(Var "f")))))
-    )
-    (Lam "u" (Var "x")))
-    (Lam "u" (Var "u"))))
+pred:: Expr->Expr 
+pred n = Lam "n" (Lam "f" (Lam "x" (
+    App (
+        App (
+            App (Var "n")(Lam "g" (Lam "h" (App(Var "h")(App (Var "g")(Var "h")))))
+        )(Lam "u" (Var "x"))
+    )(Lam "u" (Var "u")))))
