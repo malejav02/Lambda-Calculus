670adbee22922191e1299bb4037f23ae71e4622f
apalacioj <68928378+apalacioj@users.noreply.github.com>
2021-11-21 22:53:11 -0500

    Update ChurchEncoding.hs

--- church-encoding/src/ChurchEncoding.hs
+++ church-encoding/src/ChurchEncoding.hs
@@ -1,19 +1,36 @@
+-- Implementation of the Church numerals and the arithmetic operations of
+-- addition, multiplication, exponentiation and predeccesor.
+
+-- Maria Alejandra Vélez Clavijo y Alejandra Palacio Jaramillo.
+
+-- Windows 10.
+-- Tested with GHC 9.0.1 and QuickCheck 2.14.2
+
+
 module ChurchEncoding where
 import Numeric.Natural (Natural)
 import LambdaCalculus 
 
--- internalnatural genera las f°^n x correspondientes a cada natural n
 
+-- The internalnatural function recursively generates the application f^n x 
+-- corresponding to each natural n.
+-- This function receives a natural number and returns a type of expression
 internalchurch:: Natural-> Expr 
 internalchurch 0 = Var "x"
 internalchurch n= App (Var "f")(internalchurch (n-1))
 
--- esta funcion nos mantiene la estructura 
--- base lambda f.lambda x adicionando la estructura
--- f°^n x que se obtiene de la funciín internal natural
+
+-- The church function preserve the base structure λfx adding the structure 
+-- f^n x, given by the function internalchurch.
+-- This function receives a natural number and returns a type of expression.
 church :: Natural->Expr 
 church n = Lam "f" (Lam "x" (internalchurch n))
 
+
+-- The successor function receives an expression and returns another 
+-- expression.
+-- This function applies a β-reduction to obtein a natural number defined 
+-- from zero and succesor.
 successor:: Expr -> Expr 
 successor n =betared ( 
     App (
@@ -27,12 +44,15 @@ successor n =betared (
             Var "x")))))) 
             n)
 
+
+-- The add function receives two expressions and returns an expression.
+-- This function applies a β-reduction to sum two church numerals. It preserve
+-- the addition structure, that is λmnfx.mf(nfx). 
 add:: Expr -> Expr -> Expr 
 add m n = 
     betared (
     App (
     App (
-    
     Lam "m" (
         Lam "n" (
             Lam "f" (
@@ -44,6 +64,9 @@ add m n =
             Var "x")))))))(m))(n))
 
 
+-- The mult function receives two expressions and returns an expression.
+-- This function applies a β-reduction to multiply two church numerals. It 
+-- preserve the multiply structure, that is λmnfx.m(nf)x.
 mult:: Expr->Expr->Expr 
 mult m n=
     betared(
@@ -59,6 +82,11 @@ mult m n=
                 App (Var "n")(Var "f"))) (
                     Var "x"))))))(m))(n))
 
+
+-- The exp function receives two expressions and returns an expression.
+-- This function applies a β-reduction to do the first expression to the power 
+-- of the second expression. It preserve the exponentiation structure, that is 
+-- λmnfx.(nm)fx.
 exp:: Expr ->Expr -> Expr 
 exp m n= 
     betared(
@@ -74,6 +102,12 @@ exp m n=
               Var "f"))(Var "x"))))))(m))(n))
 
 
+-- This function receives an expression and returns an expression.
+-- The pred function calculates the predeccesor of a Church numeral. If the
+-- Church numeral represents zero, then applies a β-reduction to the number.
+-- Then, if the Church numeral is different, the function applis a β-reduction
+-- to obtein the predeccesor of n. It preserves the predeccesor structure, that
+-- is λnfx.n(λgh.h(gf))(λu.x)(λu.u). 
 pred:: Expr->Expr 
 pred n=
      if n == church 0 then betared (church 0)
@@ -93,5 +127,4 @@ else
                                 Var "f")))))
         )(Lam "u" (Var "x"))
     )(Lam "u" (Var "u"))))))n)
-
 
