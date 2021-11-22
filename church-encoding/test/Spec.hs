a4e20a1713e1109f469c5f37af6da74d7ee0c37a
apalacioj <68928378+apalacioj@users.noreply.github.com>
2021-11-21 23:43:34 -0500

    Update Spec.hs

--- church-encoding/test/Spec.hs
+++ church-encoding/test/Spec.hs
@@ -1,3 +1,12 @@
+-- Implementation of the Testing to check if the Church encoding
+-- implementation is correct.
+
+-- Maria Alejandra Vélez Clavijo y Alejandra Palacio Jaramillo.
+
+-- Windows 10.
+-- Tested with GHC 9.0.1 and QuickCheck 2.14.2
+
+
 import ChurchEncoding 
 import LambdaCalculus
 import Numeric.Natural ( Natural ) 
@@ -16,34 +25,80 @@ instance Arbitrary Natural where
     arbitrary = arbitrarySizedNatural
     shrink  = shrinkIntegral
 
--- Test para operacion sucesor, recibe un natural
--- y devuelve un booleano que indica si la funcion 
--- creada está buena.
--- utiliza la función alphaEq que nos permite saber
--- si dos Expr son iguales
--- igualamos la funcion succesor pasandole como 
---argumento el número m con church encoding, con
--- la funcion church encoding pasandole argumento m+1
 
+-- The testSucc function receives a natural number and returns a boolean value.
+-- This function checks if the successor function is correct. It applies the 
+-- alphaEq function to know if two expressions are equal. The expressions to 
+-- compare are the successor of a Church numeral calculated with the successor 
+-- function and the Church numeral plus one calculated with the church function.
+-- If they are equal, the function returns true; otherwise, returns false.
 testSucc :: Natural -> Bool
 testSucc m =alphaEq (successor (church m)) (church (m+1))
 
+
+-- The testAdd function receives two natural numbers and returns a boolean 
+-- value.
+-- This function checks if the add function is correct. It applies the alphaEq
+-- function to know if two expressions are equal. The expressions to compare
+-- are the addition of two Church numerals calculated with the add function
+-- and the Church numeral obtained by the sum of the two initial naturals.
+-- If they are equal, the function returns true; otherwise, returns false.
 testAdd :: Natural -> Natural -> Bool
 testAdd m n  = alphaEq (add (church m ) (
-                church n )) ( church (m+n))
+                church n )) (church (m+n))
 
+
+-- The testMult function receives two natural numbers and returns a boolean 
+-- value.
+-- This function checks if the mult function is correct. It applies the alphaEq
+-- function to know if two expressions are equal. The expressions to compare
+-- are the multiplication of two Church numerals calculated with the mult 
+-- function and the Church numeral obtained by the multplication of the two 
+-- initial naturals.
+-- If they are equal, the function returns true; otherwise, returns false.
 testMult :: Natural -> Natural -> Bool
-testMult m n  =alphaEq( mult (church m ) (church n )) (
+testMult m n  =alphaEq(mult (church m ) (church n )) (
                 church (m*n))
 
+
+-- The testExp function receives two natural numbers and returns a boolean 
+-- value.
+-- This function checks if the exp function is correct. It applies the alphaEq
+-- function to know if two expressions are equal. The expressions to compare
+-- are the exponentiation of two Church numerals calculated with the exp 
+-- function and the Church numeral obtained by the first number, raised to the
+-- second number. 
+-- If they are equal, the function returns true; otherwise, returns false.
 testExp :: Natural -> Natural -> Bool
 testExp m n  = alphaEq(ChurchEncoding.exp (church m ) (
-                church n )) ( church (m^n))
+                church n )) (church (m^n))
 
+
+-- The testPred function receives a natural number and returns a boolean 
+-- value.
+-- This function checks if the pred function is correct. It applies the alphaEq
+-- function to know if two expressions are equal. The expressions to compare
+-- are the predeccessor of the natural number plus one, calculated with the 
+-- pred function and the β-reduction of the n Church numeral.
+-- If they are equal, the function returns true; otherwise, returns false.
 testPred :: Natural  -> Bool
 testPred n = alphaEq(ChurchEncoding.pred (
             church (n+1) ) )(betared( church n))
 
+ 
+-- The test function receives two natural numbers and returns a boolean value.
+-- This function confirms if all the last functions are correct, checking with
+-- the given arguments. 
+-- If they are correct, the function returns true; otherwise, returns false.
+test:: Natural -> Natural -> Bool 
+test m n= (testSucc m)&& (testSucc n) && (testAdd m n) && (
+          testMult m n)&& (testPred m) && (testPred n)
+
+          
+main:: IO()
+main = do 
+    quickCheck $ test 
+    (quickCheckWith stdArgs{maxSuccess = 100, maxSize=9}) $ testExp 
  
 test:: Natural -> Natural -> Bool 
 test m n= (testSucc m)&& (testSucc n) && (testAdd m n) && (
